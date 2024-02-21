d5a43331c3fcbeca0dcb5a8c577d16dbef2fe8a3
EricaMarzi <erica.marzi.1993@gmail.com>
2024-02-21 17:28:23 +0100

    merge and randomize array

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
+<?php 
+
+$alphabeth = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+$numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+$symbols = ['!', '£', '%', '&', '/', '#', '@', '*', '§', 'ù', 'ç'];
+
+// Unire gli array
+$characters = array_merge($alphabeth, $numbers, $symbols);
+// var_dump($characters);
+
+$new_password = array_rand($characters, 10);
+var_dump($new_password);
+
+
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
