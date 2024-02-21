b8b5b37a41a9f50f84cea41a7543600cf1a82c8c
EricaMarzi <erica.marzi.1993@gmail.com>
2024-02-21 20:19:51 +0100

    style page password

--- script.php
+++ script.php
@@ -16,8 +16,28 @@ include __DIR__ . '/function/function.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    
+    <!--Bootstrap-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <!--CSS-->
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <p>  <?= $new_password ?> </p>
+    <div class="container-sm text-center my-5">
+
+        <header>
+            <h1 class="title mb-3">La tua password</h1>
+        </header>
+        <main>
+            <div class="card p-5">
+                <p class="new-password text-center">  <?= $new_password ?> </p>
+                <form action="index.html">
+                    <button type="submit" class="btn-style">Back</button>
+                </form>
+            </div>
+        </main>
+    </div>
 </body>
 </html>
